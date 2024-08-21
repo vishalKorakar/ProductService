2d089ce5545bee9223afd49f735ca2f7f3667cd0
KoGaVishal <vishalmarvel007@gmail.com>
2024-08-21 14:28:13 +0530

    implemented the cardinalities and relationship between product and category model (table)

--- src/main/java/com/scalerproject/psmicroservice/productservice/model/Category.java
+++ src/main/java/com/scalerproject/psmicroservice/productservice/model/Category.java
@@ -1,12 +1,17 @@
 package com.scalerproject.psmicroservice.productservice.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.util.List;
 
 @Getter
 @Setter
@@ -14,4 +19,14 @@ import java.io.Serializable;
 @NoArgsConstructor
 public class Category extends BaseModel implements Serializable {
     private String title;
+
+    /**
+     * The below can be included based on the user requirement for fetching lists of products from the db
+     * for a category specified. This can also be fetched by going to product table and specifying the category name.
+     * The below will reduce that query timing for the above idea.
+     */
+
+    @JsonIgnore // This prevents the result from going a infinite loop. this is because each category have products and each products model json has a category which in turn gets the category of the same.
+    @OneToMany(mappedBy = "category", cascade = {CascadeType.REMOVE}) // @OneToMany(mappedBy = "category") This is used to make spring understand that the relationships are the same.if we do not mention this then hibernate creates a new FK for the below and the relation specified in the product model
+    List<Product> productList;
 }
--- src/main/java/com/scalerproject/psmicroservice/productservice/model/Product.java
+++ src/main/java/com/scalerproject/psmicroservice/productservice/model/Product.java
@@ -2,7 +2,9 @@ package com.scalerproject.psmicroservice.productservice.model;
 
 
 import com.fasterxml.jackson.databind.ser.Serializers;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -19,5 +21,6 @@ public class Product extends BaseModel implements Serializable {
     private double price;
     private String imageURL;
 
+    @ManyToOne(cascade = {CascadeType.PERSIST}) // Even if we delete the products from here the category table does not get affected.
     private Category category; //this is a field from category model
 }
